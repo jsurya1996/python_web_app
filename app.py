0590d1f1a8c136c04651b879f3cd11560de92df6
jsurya1996 <137191431+jsurya1996@users.noreply.github.com>
2024-01-07 18:36:25 +0530

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return "Welcome, to python Web Application"
+    return "Welcome, to Surya first  Web Application"
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0',port=3000)
